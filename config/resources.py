e6476b972adb3a738a6267f8641670aa7dc5c20d
tungbt94 <tungbt94@gmail.com>
2017-02-09 01:10:02 +0700

    update get hotel resource

--- config/resources.py
+++ config/resources.py
@@ -9,7 +9,8 @@ from controller.RegisterRes import RegisterRes
 def config_resources(app):
     api = Api(app)
     api.add_resource(HotelListRes, "/api/hotels")
-    api.add_resource(HotelRes, "/api/hotel/<hotel_id>")
+    api.add_resource(HotelRes, "/api/hotel/<hotel_id>", "/api/hotel/<latitude>/<longitude>",
+                     "/api/hotel/top/<rate>/<number>")
 
     api.add_resource(RegisterRes, "/api/register")
     api.add_resource(LoginRes, "/api/login")
--- controller/HotelRes.py
+++ controller/HotelRes.py
@@ -1,7 +1,12 @@
+import math
+import operator
+from itertools import count
+
 from flask.ext.restful import Resource
 from flask.ext.restful import reqparse
 
 from config import mlab
+
 from models.Hotel import Hotel
 
 parser = reqparse.RequestParser()
@@ -15,10 +20,37 @@ parser.add_argument("image_url", type=str, location="json")
 
 
 class HotelRes(Resource):
+    # get hotel with id
     def get(self, hotel_id):  # get a hotel
         all_hotels = Hotel.objects
         found_hotel = all_hotels.with_id(hotel_id)
         return mlab.item2json(found_hotel)
+
+    # get hotel around point with latitude and longitude
+    def get(self, latitude, longitude):
+        max_distance = 30
+        all_hotels = Hotel.objects
+        near_hotels = []
+        flatitude = float(latitude)
+        flongitude = float(longitude)
+        for hotel in all_hotels:
+            latitude_distance = math.fabs(hotel['latitude'] - flatitude)
+            longitude_distance = math.fabs(hotel['longitude'] - flongitude)
+            if latitude_distance <= max_distance and longitude_distance <= max_distance:
+                near_hotels.append(hotel)
+        return mlab.list2json(near_hotels)
+
+    # get number of top hotel with rate
+    def get(self, rate, number):
+        all_hotels = Hotel.objects
+        result_hotels = []
+        frate = float(rate)
+        inumber = int(number)
+        for hotel in all_hotels:
+            if hotel['rating'] >= frate:
+                result_hotels.append(hotel)
+        top_hotels = sorted(result_hotels, key=operator.itemgetter('rating'))[:inumber]
+        return mlab.list2json(top_hotels)
 
     def delete(self, hotel_id):
         all_hotels = Hotel.objects
@@ -40,10 +72,3 @@ class HotelRes(Resource):
         found_hotel.update(set__longitude=longitude, set__latitude=latitude, set__telephone_number=telephone_number,
                            set__name=name, set__rating=rating, set__address=address, set__image_url=image_url)
         return {"code": 1, "status": "Ok"}, 200
-
-
-
-
-# TODO:
-# 1. get hotel around long and lat
-# 2. get hotel with rate >= a rate
